9706c324b52245f3c35ef4381a0c18504c61cb8d
Vincent li <aifreshdev@gmail.com>
2018-01-10 22:06:35 +0800

    add json mapping project.

---
+++ JsonMapDemo/JsonMapDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonMapDemo
+//
+//  Created by Vincent li on 10/1/2018.
+//  Copyright © 2018年 Vincent li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
